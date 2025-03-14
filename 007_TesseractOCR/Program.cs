d4426375a1e44255b1fa04487380d822445f3b9b
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2025-03-14 10:19:35 +0300

    Text Extraction

---
+++ 007_TesseractOCR/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using Tesseract;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Karakter okuması yapılacak resim yolu :");
+        string imagePath = Console.ReadLine();
+        Console.WriteLine();
+
+        string tessDataPath = @"C:\tessdata";
+
+        try
+        {
+            // TesseractEngine : OCR motorunu başlatır.
+            using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+            {
+                // Pix : tesseract için resimleri işlenebilir hale getirir.
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine("Okunan metin");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Bir hata meydana geldi : {e.Message}");
+        }
+        Console.ReadLine();
+    }
+}
