622ab6ea7f9d85cee93a7a6f2ef3c3f5ff2ffd42
Ömer Faruk Gözegir <gozgirfaruk@gmail.com>
2025-03-15 13:29:46 +0300

    Text To Speech

---
+++ 010_TextToSpeech/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = 0;
+
+        Console.WriteLine("Metni Giriniz");
+        string input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+        Console.ReadLine();
+    }
+}
